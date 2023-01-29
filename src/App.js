e4fee1cedbbe28f6bb97bda887fff7c23d4a5533
SHUBHAM MEENA <120468104+SHUBHARSHY@users.noreply.github.com>
2023-01-29 18:41:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Todo from "./components/todoreact/todo";
+// import Temp from "./components/weather/temp";
+import Temp from "./temp";
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+};
+
+export default App;
